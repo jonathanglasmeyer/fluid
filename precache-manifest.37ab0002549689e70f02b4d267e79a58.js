1a4652cc6d5c1fda80d620b28bc84f06f93631e8
Jonathan Glasmeyer <jwerner@posteo.de>
2018-10-21 13:22:17 +0200

    Updates

---
+++ precache-manifest.37ab0002549689e70f02b4d267e79a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d598a1d7b3b21f143700",
+    "url": "/static/js/main.d598a1d7.chunk.js"
+  },
+  {
+    "revision": "e3f0ef215ed2c67830a8",
+    "url": "/static/js/1.e3f0ef21.chunk.js"
+  },
+  {
+    "revision": "d598a1d7b3b21f143700",
+    "url": "/static/css/main.ee1dd726.chunk.css"
+  },
+  {
+    "revision": "15e319e2e5cfa77ccc35d563eece5c03",
+    "url": "/index.html"
+  }
+];
